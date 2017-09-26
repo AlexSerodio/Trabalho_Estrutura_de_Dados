819f8956618b7eb0ee5bb7a22757e55eb21c5460
Guilherme <guilherme.pinto@govbr.com.br>
2017-09-26 13:34:34 -0300

    update

--- src/main/java/br/furb/commons/FilerProcesser.java
+++ src/main/java/br/furb/commons/FilerProcesser.java
@@ -4,15 +4,16 @@ import java.io.File;
 import java.io.IOException;
 
 import br.furb.commons.pilha.ListaEstatica;
+import br.furb.model.Tag;
 
 public class FilerProcesser {
 	public void processarArquivo(File arquivo) throws IOException {
-		ListaEstatica<String> tags = TagFinder.separaTags(CharReader.lerArquivo(arquivo));
+		ListaEstatica<String> tagList = TagFinder.separaTags(CharReader.lerArquivo(arquivo));
 
-		for (int i = 0; i < tags.getTamanho(); i++)
-			System.out.println(tags.obterElemento(i));
-		//regex
-		//model tag
-		
+		for (String tag : new Tag().getEnumNames())
+			for (int i = 1; i != tagList.getTamanho(); i++)
+				if (!tag.equals(tagList.obterElemento(i)) && !tagList.obterElemento(i).startsWith("/")) {
+
+				}
 	}
 }
